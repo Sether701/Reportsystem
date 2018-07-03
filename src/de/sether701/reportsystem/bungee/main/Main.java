7e44d0244d2382fa90fedfdac68ac3f721ac64a6
Sether701 <sether017@gmail.com>
2018-07-03 19:47:00 +0200

    Created FileManager-instance in Main and create Files

--- src/de/sether701/reportsystem/bungee/main/Main.java
+++ src/de/sether701/reportsystem/bungee/main/Main.java
@@ -1,16 +1,22 @@
 package de.sether701.reportsystem.bungee.main;
 
+import de.sether701.reportsystem.bungee.filemanagement.FileManager;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class Main extends Plugin {
 
 	private static Main plugin;
+	private static FileManager fileManager;
+	
 	public static String PREFIX;
 	public static String MODE;
 	
 	@Override
 	public void onEnable() {
 		plugin = this;
+		
+		fileManager = new FileManager();
+		fileManager.createFiles();
 		
 	}
 	
