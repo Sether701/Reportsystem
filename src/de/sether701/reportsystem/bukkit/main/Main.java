29a7815ea6f44391b498bb32f16c2c0947465a0a
Sether701 <sether017@gmail.com>
2018-07-03 10:15:11 +0200

    created Bukkit-Main and Main-Instance

---
+++ src/de/sether701/reportsystem/bukkit/main/Main.java
@@ -0,0 +1,23 @@
+package de.sether701.reportsystem.bukkit.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	private static Main plugin;
+	
+	@Override
+	public void onEnable() {
+		plugin = this;
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	public static Main getPlugin() {
+		return plugin;
+	}
+}
