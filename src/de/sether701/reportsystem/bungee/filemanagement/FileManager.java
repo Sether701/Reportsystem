94ae41df91691ab678cf75e61efd0a6c7ce12f6d
Sether701 <sether017@gmail.com>
2018-07-06 21:55:03 +0200

    Removed filepaths and fix some mistakes

--- src/de/sether701/reportsystem/bungee/filemanagement/FileManager.java
+++ src/de/sether701/reportsystem/bungee/filemanagement/FileManager.java
@@ -19,32 +19,6 @@ public class FileManager {
 	private MySQL mysql;
 	private boolean mysqlEnabled;
 	
-	private static final String SETTINGS_PATH_PREFIX = "prefix",
-								SETTINGS_PATH_MODE = "mode",
-								SETTINGS_PATH_LANGUAGE = "language",
-								SETTINGS_PATH_MYSQL = "mysql",
-								
-								MYSQL_PATH_USER = "user",
-								MYSQL_PATH_PASSWORD = "password",
-								MYSQL_PATH_DATABASE = "database",
-								MYSQL_PATH_HOST = "host",
-								MYSQL_PATH_PORT = "port";
-	
-	public static final String LANG_UNSUCCESSFUL_PLUGINSTART = "console.unsuccessful_pluginstart",
-						  		LANG_SUCCESSFUL_PLUGINSTART = "console.successful_pluginstart",        
-						  		LANG_CONFIG_MODE_none_1 = "console.config_mode_none_1",
-						  		LANG_CONFIG_MODE_none_2 = "console.config_mode_none_2",
-						  		LANG_CONFIG_MODE_INVALID = "console.config_mode_invalid",
-						  		LANG_MYSQL_INVALID = "console.mysql_invalid";
-	
-	@SuppressWarnings("serial")
-	private final ArrayList<String> modes = new ArrayList<String>() {{
-		add("none"); 
-		add("custom"); 
-		add("list"); 
-		add("gui");
-	}};
-	
 	public void createFiles() {
 		
 		String path = BungeeMain.getPlugin().getDataFolder().getPath();
@@ -56,28 +30,34 @@ public class FileManager {
 		
 		/* settings */
 		
-		settingsYaml = new Yaml(path, "config.yml");
+		this.settingsYaml = new Yaml(path, "config.yml");
 		
-		BungeeMain.PREFIX = ChatColor.translateAlternateColorCodes('&', settingsYaml.read(SETTINGS_PATH_PREFIX) + " &r");
-		String mode = (String) settingsYaml.read(SETTINGS_PATH_MODE);
-		if(modes.contains(mode)) {
-			BungeeMain.MODE = mode;
-		} else {
-			BungeeMain.MODE = "invalid";
-		}
+		BungeeMain.PREFIX = ChatColor.translateAlternateColorCodes('&', (settingsYaml.read(FilePath.SETTINGS_PATH_PREFIX) + " &r"));
+		String mode = (String) settingsYaml.read(FilePath.SETTINGS_PATH_MODE);
+		BungeeMain.MODE = mode;
 		
 		/* language */
 		
-		String lang = (String) settingsYaml.read(SETTINGS_PATH_LANGUAGE);
+		String lang = (String) settingsYaml.read(FilePath.SETTINGS_PATH_LANGUAGE);
 		if(!(lang.equalsIgnoreCase("de") || lang.equalsIgnoreCase("en"))) lang = "en";
-		languageYaml = new Yaml(path, "lang_"+lang+".yml");
+		this.languageYaml = new Yaml(path, "lang_"+lang+".yml");
 		
 		/* permissions */
 		
-		permissionsYaml = new Yaml(path, "permissions.yml");
+		this.permissionsYaml = new Yaml(path, "permissions.yml");
+		
+		/* settings */
+		
+		switch (mode) {
+			case "none": return;
+			case "gui": break;
+			case "list": break;
+			case "custom": break;
+			default: BungeeMain.MODE = "invalid"; return;
+		}
 		
 		/* mysql */
-		mysqlEnabled = Boolean.valueOf((String) settingsYaml.read(SETTINGS_PATH_MYSQL));
+		this.mysqlEnabled = Boolean.valueOf((String) settingsYaml.read(FilePath.SETTINGS_PATH_MYSQL));
 		Yaml mysqlYaml = new Yaml(path, "mysql.yml");
 		if(mysqlEnabled) {
 			ArrayList<MySQLTable> tables = new ArrayList<>();
@@ -108,11 +88,11 @@ public class FileManager {
 			tables.add(archive);
 			tables.add(reasons);
 			
-			mysql = new MySQL((String) mysqlYaml.read(MYSQL_PATH_USER),
-								(String) mysqlYaml.read(MYSQL_PATH_PASSWORD), 
-								(String) mysqlYaml.read(MYSQL_PATH_DATABASE), 
-								(String) mysqlYaml.read(MYSQL_PATH_HOST), 
-								(String) mysqlYaml.read(MYSQL_PATH_PORT), 
+			this.mysql = new MySQL((String) mysqlYaml.read(FilePath.MYSQL_PATH_USER),
+								(String) mysqlYaml.read(FilePath.MYSQL_PATH_PASSWORD), 
+								(String) mysqlYaml.read(FilePath.MYSQL_PATH_DATABASE), 
+								(String) mysqlYaml.read(FilePath.MYSQL_PATH_HOST), 
+								(String) mysqlYaml.read(FilePath.MYSQL_PATH_PORT), 
 								tables);
 			
 			if(!mysql.connect()) {
